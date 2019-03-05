44968f3de0dfc5e199959a1efeb009092e984f64
Chris Mika <chris.mika@nymag.com>
2019-03-05 14:54:13 -0500

    auto create reverse zone

--- .createReverseZone.py
+++ .createReverseZone.py
@@ -3,57 +3,40 @@
 # update config script to limit allow-query to reverse zone
 
 import dns.zone
-import dns.ipv4
 import dns.resolver
 import dns.tsigkeyring
 import re
 from shutil import copyfile
-import pprint
 
 reverseDNS = dns.resolver.Resolver()
 reverseDNS.nameservers=["69.125.235.157", "168.235.68.77", "52.201.141.242"]
 keyring = dns.tsigkeyring.from_text({"query-key":"kVhxznJq5fgHn961Am7EZw=="})
 reverseDNS.use_tsig(keyring, keyname="query-key")
-serial = reverseDNS.query("168.192.in-addr.arpa", "SOA").rrset[0].serial
-print serial
+serial = reverseDNS.query("168.192.in-addr.arpa", "SOA")[0].serial + 1
 
+zoneFileName = "db.168.192.in-addr.arpa"
+copyfile(".db.template", zoneFileName)
 
+entries = []
+zone = dns.zone.from_file('db.seenothing.net')
+for (name, ttl, rdata) in zone.iterate_rdatas('A'):
+  if re.match(r"^192.168.", rdata.to_text()):
+    entry = {
+      "thirdOctet": rdata.to_text().split(".")[2],
+      "fourthOctet": rdata.to_text().split(".")[3],
+      "name": name.to_text()
+    }
+    entries.append(entry)
 
-# below, the bottom two are equivalent, the [0] is the first response in a list, there's only one list member
-resp = reverseDNS.query("168.192.in-addr.arpa", "SOA")
-print resp.rrset[0].serial
-type (resp.rrset[0].serial)
-print hex(id(resp.rrset[0].serial))
-print resp[0].serial
-type (resp[0].serial)
-try:
-  print resp[1].serial
-except Exception as e:
-  print e
-print hex(id(resp[0].serial))
-resp = reverseDNS.query("seenothing.org", "MX")
-print resp.rrset[0].preference
-print resp.rrset[1].preference
+with open(zoneFileName, "r") as sources:
+  lines = sources.readlines()
+with open(zoneFileName, "w") as sources:
+  for line in lines:
+    line = re.sub(r"<SERIAL>", str(serial), line)
+    line = re.sub(r"<DOMAIN>", zoneFileName.partition(".")[2], line)
+    sources.write(line)
 
-
-
-print type(reverseDNS.query("168.192.in-addr.arpa", "SOA"))
-print vars(reverseDNS.query("168.192.in-addr.arpa", "SOA"))
-print "#####"
-print type(reverseDNS.query("168.192.in-addr.arpa", "SOA").__class__)
-pprint.pprint(reverseDNS.query("168.192.in-addr.arpa", "SOA")[0])
-print "#####"
-print type(reverseDNS.query("168.192.in-addr.arpa", "SOA")[0].serial)
-
-
-# zoneFileName = "db.168.192.in-addr.arpa"
-# copyfile(".db.template", zoneFileName)
-# zoneFile = open(zoneFileName, "w")
-# buffer = "\n;\n; PTR records\n; ------------------------------X---------------X-----\n"
-
-# zone = dns.zone.from_file('db.seenothing.net')
-# for (name, ttl, rdata) in zone.iterate_rdatas('A'):
-#   if re.match(r"^192.168.", rdata.to_text()):
-#    ip = rdata.to_text().split(".")
-#    buffer += ip[3] + "." + ip[2] + "\t\t\t\tIN PTR\t\t" + name.to_text() + "\n"
-# print buffer
+sources = open(zoneFileName, "a")
+sources.write("\n;\n; PTR records\n; ------------------------------X---------------X-----\n")
+for i in sorted(entries, key=lambda k: (int(k["thirdOctet"]), int(k["fourthOctet"]))):
+  sources.write(i["fourthOctet"] + "." + i["thirdOctet"] + "\t\t\t\tIN PTR\t\t" + i["name"] + "\n")
